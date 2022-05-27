23a16207a0c0f5391da41232e829669211686c99
sbarsocchini <sbarsocchini@shiftlabor.com>
2022-05-27 18:12:45 +0100

    test cn

---
+++ WEBTEST/WEBTEST/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEBTEST.Startup))]
+namespace WEBTEST
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
